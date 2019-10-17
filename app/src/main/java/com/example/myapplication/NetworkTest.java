18510bd1c0bfeae8d5b54cdb1ef3b6b6a5561d9a
zhangyi <1104745049@qq.com>
2019-10-17 14:52:37 +0800

    添加网络测试

---
+++ app/src/main/java/com/example/myapplication/NetworkTest.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NetworkTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_test);
+    }
+}
